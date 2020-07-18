ac8cacd737f7dfec50250bccdc8b04263ed7e85b
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-07-18 23:43:50 +0530

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Row } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
@@ -13,13 +14,17 @@ const minLength = (len) => (val) => val && (val.length >= len);
     if(dish!=null) {
       return(
         <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-            <CardBody>
-              <CardTitle>{dish.name}</CardTitle>
-              <CardText>{dish.description}</CardText>
-            </CardBody>
-          </Card>
+          <FadeTransform in transformProps ={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+          }}>
+            <Card>
+              <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+              <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                <CardText>{dish.description}</CardText>
+              </CardBody>
+            </Card>
+          </FadeTransform>
         </div>
       );
     } else {
@@ -37,17 +42,19 @@ const minLength = (len) => (val) => val && (val.length >= len);
     }
     const comment = comments.map(comment => {
       return(
-        <li key={comment.id}>
-          <p>{ comment.comment }</p>
-          <p>--{ comment.author},
-          &nbsp;
-          {new Intl.DateTimeFormat('en-US', {
-            year: 'numeric',
-            month: 'long',
-            date: '2-digit'
-          }).format(new Date(comment.date))}
-          </p>
-        </li>
+        <Fade in>
+          <li key={comment.id}>
+            <p>{ comment.comment }</p>
+            <p>--{ comment.author},
+            &nbsp;
+            {new Intl.DateTimeFormat('en-US', {
+              year: 'numeric',
+              month: 'long',
+              date: '2-digit'
+            }).format(new Date(comment.date))}
+            </p>
+          </li>
+        </Fade>
       )
     });
     comment.push(<CommentForm dishId={dishId} postComment={postComment}></CommentForm>)
@@ -55,7 +62,9 @@ const minLength = (len) => (val) => val && (val.length >= len);
       <div className="col-12 col-sm-5 m-1">
         <h4>Comments</h4>
         <ul className="list-unstyled">
-          {comment}
+          <Stagger in>
+            {comment}
+          </Stagger>
         </ul>
       </div>
     );
